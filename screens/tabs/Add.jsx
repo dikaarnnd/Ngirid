61c5abe4511cd2a2b72b0e73c0a6f6d3f632cc26
dikaarnnd <dika4kiga@gmail.com>
2025-05-31 19:24:59 +0700

    first commit fe-add

--- screens/tabs/Add.jsx
+++ screens/tabs/Add.jsx
@@ -1,10 +1,17 @@
-// screens/Add.jsx
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image, TextInput, ScrollView, Alert } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { useState, useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Add() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Add Screen</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View className="flex-1 justify-center items-center">
+          <Text>Add Screen</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
